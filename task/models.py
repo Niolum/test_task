2bbf75937348b67da0b7ce8ed96cf904d7d6714b
Niolum <al_belov98@mail.ru>
2023-08-09 14:58:05 +0700

    add model Task

--- task/models.py
+++ task/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Task(models.Model):
+    """Задачи"""
+    title = models.CharField('Заголовок задачи', max_length=255)
+    description = models.TextField('Описание задачи')
+    completed = models.BooleanField('Статус задачи', default=False)
+    created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('task-detail', kwargs={'pk': self.pk})
+    
